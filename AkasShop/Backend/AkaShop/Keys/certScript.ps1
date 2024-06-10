fc3a4542c0f2332f95d1b4aea6d49a50e12ee1a1
akaki ujarashvili <ujarashvilikhatia7@gmail.com>
2024-06-10 16:03:18 +0400

    ignore cert command

--- AkasShop/Backend/AkaShop/Keys/certScript.ps1
+++
@@ -1,4 +0,0 @@
-$cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject "CN=AkaShop API" -KeyAlgorithm RSA -KeyLength 2048 -CertStoreLocation "Cert:\CurrentUser\My" -NotAfter 01/01/2033
-$pwd = ConvertTo-SecureString -String 'akashop123' -Force -AsPlainText
-Export-PfxCertificate -cert $cert -FilePath  C:\Temp\akashopcert.pfx -Password $pwd
-$cert
