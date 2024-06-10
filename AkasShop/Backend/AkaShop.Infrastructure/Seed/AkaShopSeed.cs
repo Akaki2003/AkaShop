288530ba9fd20f8da7b95d9f12c1a6aae5956afd
akaki ujarashvili <ujarashvilikhatia7@gmail.com>
2024-06-10 19:24:55 +0400

    small modification

--- AkasShop/Backend/AkaShop.Infrastructure/Seed/AkaShopSeed.cs
+++ AkasShop/Backend/AkaShop.Infrastructure/Seed/AkaShopSeed.cs
@@ -55,11 +55,11 @@ namespace AkaShop.Infrastructure.Seed
         {
             var products = new List<Product>
             {
-                new () { Name = "Fancy Hat", Price = 1500, Description = "A stylish hat for any occasion.", ImageUrl = "https://images.pexels.com/photos/324730/pexels-photo-324730.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", UserId = 1},
-                new () { Name = "Vintage Camera", Price = 2500, Description = "Capture memories with this classic camera.", ImageUrl = "https://images.pexels.com/photos/408508/pexels-photo-408508.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", UserId = 1},
-                new () { Name = "Artisanal Coffee Maker", Price = 3500, Description = "Brew the perfect cup with this handcrafted coffee maker.", ImageUrl = "https://images.pexels.com/photos/209151/pexels-photo-209151.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", UserId = 1},
-                new () { Name = "Designer Sneakers", Price = 4500, Description = "Step out in style with these trendy sneakers.", ImageUrl = "https://images.pexels.com/photos/2845548/pexels-photo-2845548.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", UserId = 1},
-                new () { Name = "Handmade Jewelry Set", Price = 5500, Description = "Elevate your look with this exquisite jewelry set.", ImageUrl = "https://images.pexels.com/photos/106156/pexels-photo-106156.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", UserId = 1},
+                new () { Name = "Fancy Hat", Price = 15, Description = "A stylish hat for any occasion.", ImageUrl = "https://images.pexels.com/photos/7084527/pexels-photo-7084527.jpeg?auto=compress&cs=tinysrgb&w=600", UserId = 1},
+                new () { Name = "Vintage Camera", Price = 25, Description = "Capture memories with this classic camera.", ImageUrl = "https://images.pexels.com/photos/37397/camera-old-antique-voigtlander.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", UserId = 1},
+                new () { Name = "Artisanal Coffee Maker", Price = 35, Description = "Brew the perfect cup with this handcrafted coffee maker.", ImageUrl = "https://images.pexels.com/photos/4993062/pexels-photo-4993062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", UserId = 1},
+                new () { Name = "Designer Sneakers", Price = 45, Description = "Step out in style with these trendy sneakers.", ImageUrl = "https://images.pexels.com/photos/1750045/pexels-photo-1750045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", UserId = 1},
+                new () { Name = "Handmade Jewelry Set", Price = 55, Description = "Elevate your look with this exquisite jewelry set.", ImageUrl = "https://images.pexels.com/photos/1395306/pexels-photo-1395306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", UserId = 1},
             };
 
             if (!context.Products.Any())
